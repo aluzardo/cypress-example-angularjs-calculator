1782d9354a29747215142dc9d90acd0c008cb072
aluzardo <adolfoluzardocabrera@gmail.com>
2016-10-13 14:31:45 +0200

    Following @fvanwijk recommendation to avoid using fixtures

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -4,33 +4,29 @@
 // ***********************************************
 
 describe("Calculator - AngularJS", function () {
-    // Set global vars
-    var calculatorJSON = null;
 
     beforeEach(function () {
-        cy.fixture("calculator.json").then(function(json){
-            calculatorJSON = json;
-            // Go out and visit the protractor demo calculator
-            // https://on.cypress.io/api/visit
-            cy.visit(calculatorJSON.url);
-        })
+
+        // Go out and visit the protractor demo calculator
+        // https://on.cypress.io/api/visit
+        cy.visit(calculatorPageObject.url);
     });
 
     context("When page is initially opened", function () {
         it("The title should be Super Calculator", function () {
             // get the currently title
             //https://docs.cypress.io/v1.0/docs/title
-            cy.title().should("contain", calculatorJSON.title)
+            cy.title().should("contain", calculatorPageObject.title)
         })
     });
 
     context("Calculator should add numbers", function () {
         it("should add one and two", function () {
             cy
-                .get(calculatorJSON.locators.inputNumber1).type('1')
-                .get(calculatorJSON.locators.inputNumber2).type('2')
-                .get(calculatorJSON.locators.goButton).click()
-                .get(calculatorJSON.locators.result).should("contain", '3')
+                .get(calculatorPageObject.locators.inputNumber1).type('1')
+                .get(calculatorPageObject.locators.inputNumber2).type('2')
+                .get(calculatorPageObject.locators.goButton).click()
+                .get(calculatorPageObject.locators.result).should("contain", '3')
         })
     })
 
---
+++ cypress/support/calculator.js
@@ -0,0 +1,10 @@
+var calculatorPageObject = {
+    "title": "Super Calculator",
+    "url": "https://aluzardo.github.io/protractor-demo/",
+    "locators": {
+        "inputNumber1": "[ng-model='first']",
+        "inputNumber2": "[ng-model='second']",
+        "goButton": "[id='gobutton']",
+        "result": "h2[class = 'ng-binding']"
+    }
+};
